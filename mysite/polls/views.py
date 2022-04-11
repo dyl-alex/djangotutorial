11449fa07bacc723430c0390e2cafc804ee617c7
Dylan <dalexander_02@arcadia.edu>
2022-04-11 09:40:00 -0400

    test

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Fuck yo mama")
+    return HttpResponse("this is a poll website")
